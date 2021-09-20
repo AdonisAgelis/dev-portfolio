c37fa14b991e8e7d4865ad8704edd03be45368f4
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-09-21 02:40:53 +0300

    Styled Components #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// import Global Style from styled-components
+import GlobalStyle from './components/GlobalStyle';
 // import Pages
 import AboutUs from './pages/AboutUs';
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
