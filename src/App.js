1624cc9a3a616af101dac28d1dd6390235d79fd9
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-10-31 19:36:22 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
 // import Global Style from styled-components
 import GlobalStyle from './components/GlobalStyle';
 // import Pages
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 
 const App = () => {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 };
