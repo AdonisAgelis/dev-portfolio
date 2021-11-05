1de1cabc2627c68414ff67c4aab89816edfad95a
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-11-05 23:03:08 +0200

    About Section Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,7 @@ import home1 from '../img/home1.png';
 import { About, Description, Hide, Image } from '../styles';
 // Framer motion
 import { motion } from 'framer-motion';
+import { titleAnimation, fade, photoAnimation } from '../animation';
 
 const AboutSection = () => {
   return (
@@ -11,25 +12,25 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Camera guy" />
+        <motion.img variants={photoAnimation} src={home1} alt="Camera guy" />
       </Image>
     </About>
   );
