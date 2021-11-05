dcb09b13c2c1afb1665b0bd560e5bbb257165b11
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-11-05 22:36:50 +0200

    Page Transitions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 // import Global Style from styled-components
 import GlobalStyle from './components/GlobalStyle';
 // import Pages
@@ -9,26 +9,32 @@ import Nav from './components/Nav';
 import OurWork from './pages/OurWork';
 import ContactUs from './pages/ContactUs';
 import MovieDetail from './pages/movieDetail';
+// Animation
+import { AnimatePresence } from 'framer-motion';
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 };
